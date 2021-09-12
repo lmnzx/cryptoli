8c23cdf5b3ff8b6bdd6a3836824b9d240d054028
Sayan Mallick <snyxmk@gmail.com>
2021-09-12 09:22:32 +0530

    initial setup

---
+++ source/components/help.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+import { Text } from "ink";
+
+const Help: FC = () => (
+	<Text>
+		Hello, <Text color="green">help</Text>
+	</Text>
+);
+
+module.exports = Help;
+export default Help;
