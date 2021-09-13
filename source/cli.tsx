2cead83968a942568db556aad386c4feb393dc65
Sayan Mallick <snyxmk@gmail.com>
2021-09-13 10:11:03 +0530

    Basic Setup with Data fetching with axios

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,54 +1,37 @@
 #!/usr/bin/env node
-// import { render } from "ink";
-// import Help from "./components/help";
-import meow from "meow";
 import chalk from "chalk";
-// import App from "./ui";
-// import parseArge from "minimist";
-// import React from "react";
+import { render } from "ink";
+import meow from "meow";
+import React from "react";
+import Default from "./components/default";
 
-const cli = meow(
-	chalk`
-	Usage
-	  $ cryptoli {green.bold input}
+(() => {
+	const cli = meow(
+		chalk`
+		Usage
+			$ cryptoli {green.bold input}
 
-	Options
-		--help  Your name
+		Options
+			--help  Your name
 
-	Examples
-	  $ cryptoli --name=Jane
-	  Hello, Jane
-`,
-	{
-		flags: {
-			help: {
-				type: "boolean",
-				alias: "h",
+		Examples
+			$ cryptoli --name=Jane
+			Hello, Jane
+	`,
+		{
+			flags: {
+				help: {
+					type: "boolean",
+					alias: "h",
+				},
 			},
-		},
+		}
+	);
+
+	if (cli.flags.help) {
+		cli.help;
 	}
-);
-
-console.log(cli.help);
-// console.log(cli.flags.help);
-
-// render(<App name={cli.flags.name}/>);
-
-// interface ParsedArgs {
-// 	_: string[];
-// 	help?: boolean;
-// 	h?: boolean;
-// }
-
-// (() => {
-// 	const argv: ParsedArgs = parseArge(process.argv.slice(2), {
-// 		boolean: ["help"],
-// 		alias: { h: "help" },
-// 	});
-
-// 	if (argv.h) {
-// 		render(<Help />);
-// 	}
-
-// 	render(<App name={argv._[0]} />);
-// })();
+	if (cli.input.length === 0) {
+		render(<Default />);
+	}
+})();
